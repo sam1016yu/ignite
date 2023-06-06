c19f846e7bd44cfd381b9fa9f69085361434fcd3
Miao Yu <miaoyu1016@gmail.com>
2023-06-06 11:49:31 -0400

    Update JdbcThinPreparedStatementLeakTest.java
    change measurement target

--- modules/clients/src/test/java/org/apache/ignite/jdbc/thin/JdbcThinPreparedStatementLeakTest.java
+++ modules/clients/src/test/java/org/apache/ignite/jdbc/thin/JdbcThinPreparedStatementLeakTest.java
@@ -24,9 +24,10 @@ import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import org.apache.ignite.IgniteJdbcThinDriver;
 import org.apache.ignite.configuration.IgniteConfiguration;
-import org.apache.ignite.internal.util.GridDebug;
+import org.apache.ignite.internal.util.typedef.internal.U;
 import org.junit.Test;
 
 /**
@@ -54,23 +55,17 @@ public class JdbcThinPreparedStatementLeakTest extends JdbcThinAbstractSelfTest 
      */
     @Test
     public void test() throws Exception {
-        startGrid();
-        for (int i = 1; i <= 10 ; i++){
-            
+
+        for (int iters = 10000; iters <= 50000;iters+=10000){
+            log.warning("####Iters:"+iters);
+            startGrid();
             try (Connection conn = new IgniteJdbcThinDriver().connect(URL, new Properties())) {
-                // System.gc();
-                // long mem1 = memoryUsage();
                 
-                createClosePreparedStatement(conn, 5000);
+                createClosePreparedStatement(conn, iters);
 
-                long mem2 = memoryUsage();
-
-                log.warning("####Iters:"+i);
-                log.warning("####Mem:"+mem2);
-
-            }   
+            }
+            stopAllGrids();
         }
-        stopAllGrids();
 
     }
 
@@ -91,10 +86,8 @@ public class JdbcThinPreparedStatementLeakTest extends JdbcThinAbstractSelfTest 
                 rs.close();
             }
         }
+        Set stmts = U.field(conn, "stmts");
+        log.warning("####stmts:"+stmts.size());
     }
 
-    private long memoryUsage() {
-        Runtime runtime = Runtime.getRuntime();
-        return runtime.totalMemory() - runtime.freeMemory();     
-    }
 }
